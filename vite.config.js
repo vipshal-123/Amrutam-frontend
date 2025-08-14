4d10615ddfc1c18e5b5f5c47148e15acc65887d6
Vipshal wg <vipshal.wg@snssquare.com>
2025-08-14 23:23:34 +0530

    github actions

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Amrutam-frontend/',
   resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
